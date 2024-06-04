9118d5545d2a8d958db43ac505d9af32a9ab63b2
rafaelnielson <rafael.nielson@gmail.com>
2024-06-03 22:40:13 -0300

    Feat: first coding commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = pd.read_csv('./data/controle_pacientes_csv.csv')
+
+
+busca_nome = data['Nome completo:']
+input = input('Entre o nome da busca:')
+flag = False
+
+for i in busca_nome:
+    if i == input:
+        flag = True
+        break
+
+if flag:
+   print(f'Sucesso, nome {input} encontrado!')      
+else:
+    print('Nome não encontrado')
+
